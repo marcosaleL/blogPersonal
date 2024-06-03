50fef5052d06d0ddfc3e0082264c17cbc33671b7
Matias Santos <santos.matias.a.97@gmail.com>
2024-06-03 19:27:45 -0300

    feat: initial article UI implementation done, new look for login UI

--- blog-personal/src/App.js
+++ blog-personal/src/App.js
@@ -1,16 +1,29 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/LoginComponent';
 import Article from './Components/Article/ArticleComponent';
+import Spacer from './Components/Spacer/SpacerComponent';
+import styled from 'styled-components'
+
+const AppUI = styled.div({
+  display: 'grid',
+  flexDirection: 'row',
+})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Crear Usuario</h1>
-        <Login type="large" />
-        <Article />
+        <Spacer></Spacer>
+        <div>
+          <Login type="large" />
+          <Article />
+        </div>
+        <Spacer></Spacer>
+
       </header>
     </div>
   );
--- blog-personal/src/Components/Article/ArticleComponent.jsx
+++ blog-personal/src/Components/Article/ArticleComponent.jsx
@@ -1,11 +1,68 @@
 import React from "react";
+import styled from 'styled-components';
 
+//
+//Negro: #000000
+//Gris oscuro: #4A4A4A
+//Gris medio: #A0A0A0
+//Gris claro: #D0D0D0
+//Celeste oscuro: #6CA6CD
+//Celeste medio: #87CEEB
+//Celeste claro: #B0E0E6
+
+const TitleUI = styled.h1`
+    height: ${({ size }) => (size === 'large' ? '50px' : size === 'medium' ? '20px' : '10px')};
+    width: ${({ size }) => (size === 'large' ? '310px' : size === 'medium' ? '20vw' : '10vw')};
+    font-size: ${({ size }) => (size === 'large' ? '50px' : size === 'medium' ? '20px' : '10px')};
+    font-weight: bold;
+    margin: 20px 10px 20px 10px;
+    color: #4A4A4A;
+    border-radius: 50px;
+    text-shadow: 
+        1px 1px 0 #f36b39;
+`;
+
+
+const SubtitleUI = styled.h2`
+    height: ${({ size }) => (size === 'large' ? '30px' : size === 'medium' ? '20px' : '10px')};
+    width: ${({ size }) => (size === 'large' ? '310px' : size === 'medium' ? '20vw' : '10vw')};
+    font-size: ${({ size }) => (size === 'large' ? '30px' : size === 'medium' ? '20px' : '10px')};
+    font-weight: bold;
+    margin: 20px 10px 20px 10px;
+    display: flex;
+    justify-content: left;
+    color: #333333
+`;
+
+const ParagraphUI = styled.p`
+    height: ${({ size }) => (size === 'large' ? '510px' : size === 'medium' ? '20px' : '10px')};
+    width: ${({ size }) => (size === 'large' ? '260px' : size === 'medium' ? '20vw' : '10vw')};
+    font-size: ${({ size }) => (size === 'large' ? '15px' : size === 'medium' ? '20px' : '10px')};
+    font-weight: bold;
+    background-color: #333333;
+    border-radius: 50px;
+    padding: 25px;
+    color: #EEEEEE;
+    text-align: left; /* Aligns text to the left */
+`;
+const ArticleUI = styled.div`
+    height: 650px;
+    width: 350px;
+    background: linear-gradient(0deg, #004173 0%,  #000000 25%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 50px;
+    margin: 20px;
+`;
 function Article({ props }) {
 
     return (
-        <div>
-            <h1>Article</h1>
-        </div>
+        <ArticleUI>
+            <TitleUI size="large">Article title</TitleUI>
+            <SubtitleUI size="large">Article subtitle</SubtitleUI>
+            <ParagraphUI size="large">Cillum sint nulla mollit enim id quis ut ullamco do voluptate.Reprehenderit sunt pariatur excepteur ut aute. Ad labore incididunt tempor anim. Laborum ullamco consectetur ipsum elit amet ad Lorem ullamco. Ipsum velit dolor aute cillum tempor ea elit amet est aliqua. Voluptate pariatur aute minim irure nostrud voluptate cillum.</ParagraphUI>
+        </ArticleUI>
     );
 }
 
